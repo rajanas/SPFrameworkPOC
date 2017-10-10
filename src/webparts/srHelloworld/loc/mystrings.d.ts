b2f8d281abb444dd6b16ab1901942846182bdbd9
rajana suresh <rajana.suresh@outlook.com>
2017-10-10 12:03:05 -0700

    no js hello world

---
+++ src/webparts/srHelloworld/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISrHelloworldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SrHelloworldWebPartStrings' {
+  const strings: ISrHelloworldWebPartStrings;
+  export = strings;
+}
